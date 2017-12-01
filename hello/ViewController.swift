b08662f807e47ea8f86636d9db85858be1e2000d
Joseph Chu <jchu01@icloud.com>
2017-11-30 21:25:01 -0500

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Joseph Chu on 11/30/17.
+//  Copyright © 2017 Joseph Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
